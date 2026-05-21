cfd16321f823393ddcda16fda29b1a8731073384
chinmay7488 <chinmayjainst8314@gmail.com>
2026-05-21 19:51:18 +0530

    added recipe model. added login, logout and register views.

--- Recipe_Meal_Planner/models.py
+++ Recipe_Meal_Planner/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Recipe_Category(models.TextChoices):
+    Breakfast = 'Br', 'Breakfast'
+    Lunch = 'Lh', 'Lunch'
+    Dinner = 'Dr', 'Dinner'
+
+
+class Recipes(models.Model):
+    Person = models.ForeignKey(User, on_delete=models.CASCADE)
+    Title = models.TextField(max_length=100)
+    Description = models.TextField()
+    Prep_time = models.IntegerField()
+    Category = models.TextField(choices=Recipe_Category, default=Recipe_Category.Breakfast)
+    Recipe_Photo = models.ImageField()
+    Instruction = models.TextField()
+    Recipe_Calories = models.IntegerField()
+    Recipe_Protein = models.IntegerField()
+    Recipe_Carbs = models.IntegerField()
+    Recipe_Fats = models.IntegerField()
+
--- Recipe_Meal_Planner/views.py
+++ Recipe_Meal_Planner/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, "Recipe_Meal_Planner/index.html")
@@ -8,7 +9,33 @@ def export_Recipe(request):
     return render(request, "Recipe_Meal_Planner/export.html")
 
 def Login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        remember_me = request.POST.get('remember') == "on"
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            if remember_me:
+                request.session.set_expiry(1209600) 
+            else:
+                request.session.set_expiry(0)
+            return redirect('TAnalyzer:home')
+        else:
+            error_messages =  "Invalid username or password."
+
+        return render(request, 'Recipe_Meal_Planner/login.html', {"error_messages": error_messages})
+
     return render(request, "Recipe_Meal_Planner/login.html")
+
+def logout_page(request):
+    if request.method == "POST":
+        auth_logout(request)
+        return redirect('TAnalyzer:home')
+
+    return render(request, 'Text_Analyzer/logout.html')
 
 def Meal_Planner(request):
     return render(request, "Recipe_Meal_Planner/meal_planner.html")
@@ -32,7 +59,22 @@ def Recipes(request):
     return render(request, "Recipe_Meal_Planner/recipes.html")
 
 def Register(request):
-    return render(request, "Recipe_Meal_Planner/register.html")
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password != confirm_password:
+            return render(request, 'Recipe_Meal_Planner/register.html', {"error_messages": "Passwords do not match."})
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'Recipe_Meal_Planner/register.html', {"error_messages": "Username already taken!"})
+
+        User.objects.create_user(username=username, email=email, password=password)
+        return redirect('Recipe_Meal_Planner:login')
+
+    return render(request, 'Recipe_Meal_Planner/register.html')
 
 def Shopping_list(request):
     return render(request, "Recipe_Meal_Planner/shopping_list.html")
