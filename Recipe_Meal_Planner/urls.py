0f18d5a6b0518a8e7e6a7221928ea4262b5564ac
chinmay7488 <chinmayjainst8314@gmail.com>
2026-05-21 12:34:42 +0530

    added html pages with styling fpr recipe meal planner. Changed to jinja template for base html extends and urls pattern. create all url patterns and there views

--- Recipe_Meal_Planner/urls.py
+++ Recipe_Meal_Planner/urls.py
@@ -6,4 +6,16 @@ app_name = "Recipe_Meal_Planner"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("export/", views.export_Recipe, name="export"),
+    path("login/", views.Login_page, name="login"),
+    path("meal-planner/", views.Meal_Planner, name="meal_planner"),
+    path("nutrition/", views.Nutrition, name="nutrition"),
+    path("pantry/", views.Pantry, name="pantry"),
+    path("recipes/", views.Recipes, name="recipes"),
+    path("recipes/delete/", views.Delete_recipe, name="recipe_delete"),
+    path("recipes/detail/", views.Detail_recipe, name="recipe_detail"),
+    path("recipes/form/", views.Form_recipe, name="recipe_form"),
+    path("register/", views.Register, name="register"),
+    path("shopping-list/", views.Shopping_list, name="shopping_list"),
+    path("smart-generator/", views.Smart_Generator, name="smart_generator"),
 ]
--- Recipe_Meal_Planner/views.py
+++ Recipe_Meal_Planner/views.py
@@ -3,3 +3,39 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "Recipe_Meal_Planner/index.html")
+
+def export_Recipe(request):
+    return render(request, "Recipe_Meal_Planner/export.html")
+
+def Login_page(request):
+    return render(request, "Recipe_Meal_Planner/login.html")
+
+def Meal_Planner(request):
+    return render(request, "Recipe_Meal_Planner/meal_planner.html")
+
+def Nutrition(request):
+    return render(request, "Recipe_Meal_Planner/nutrition.html")
+
+def Pantry(request):
+    return render(request, "Recipe_Meal_Planner/pantry.html")
+
+def Delete_recipe(request):
+    return render(request, "Recipe_Meal_Planner/recipe_delete.html")
+
+def Detail_recipe(request):
+    return render(request, "Recipe_Meal_Planner/recipe_detail.html")
+
+def Form_recipe(request):
+    return render(request, "Recipe_Meal_Planner/recipe_form.html")
+
+def Recipes(request):
+    return render(request, "Recipe_Meal_Planner/recipes.html")
+
+def Register(request):
+    return render(request, "Recipe_Meal_Planner/register.html")
+
+def Shopping_list(request):
+    return render(request, "Recipe_Meal_Planner/shopping_list.html")
+
+def Smart_Generator(request):
+    return render(request, "Recipe_Meal_Planner/smart_generator.html")
